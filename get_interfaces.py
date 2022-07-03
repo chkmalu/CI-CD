185090ef172a24baeffce92e9b6138d9ab56eddc
malu <chikamaluj@gmail.com>
2022-07-04 00:37:15 +0100

    user_env_variables

--- get_interfaces.py
+++ get_interfaces.py
@@ -2,14 +2,14 @@ from netmiko import ConnectHandler
 import os
 
 
-# user = os.environ["USERNAME"]
+user = os.environ["USERNAME"]
 password = os.environ["PASSWORD"]
 
 router = {
     "device_type": "cisco_ios",
     "ip": "192.168.117.10",
-    "username": 'malu',
-    "password": 'Password1234'
+    "username": user,
+    "password": password
 }
 
 net_connect = ConnectHandler(**router)
