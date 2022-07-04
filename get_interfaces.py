f78bd7874f0dd2b8205138d78f3d6f9dfb6e780e
malu <chikamaluj@gmail.com>
2022-07-05 00:08:49 +0100

    try_again

--- get_interfaces.py
+++ get_interfaces.py
@@ -1,19 +1,17 @@
 import os
 
-
 from netmiko import ConnectHandler
 
-
-user = os.environ["USERNAME"]
+user = os.environ["USER"]
 password = os.environ["PASSWORD"]
 
-router = {
+iosv_l2_s1 = {
     "device_type": "cisco_ios",
     "ip": "192.168.117.10",
     "username": user,
-    "password": password
+    "password": password,
 }
 
-net_connect = ConnectHandler(**router)
+net_connect = ConnectHandler(**iosv_l2_s1)
 output = net_connect.send_command("show ip int brief")
 print(output)
