6c09e52297fa18280823bc8c995fb0e714806f92
malu <chikamaluj@gmail.com>
2022-07-04 20:13:34 +0100

    testing

--- get_interfaces.py
+++ get_interfaces.py
@@ -12,6 +12,6 @@ router = {
     "password": password
 }
 
-net_connect = ConnectHandler(**router)
+net_connec = ConnectHandler(**router)
 output = net_connect.send_command("show ip int brief")
 print(output)
